67d0d54a38d61b4ec3149b4c6c4113fc34f6d69d
David Merfield <dmerfield@gmail.com>
2014-04-09 13:31:57 -0700

    restructured color generator to match spec

--- randomColor.js
+++ randomColor.js
@@ -1,135 +1,168 @@
-var randomColor = function randomColor(options){
+function randomColor (options) {
 
-  var h,s,v;
+  console.log(options);
+  
+  var h,s,v,l,
+      rgb,hex,
 
-  // I'm using the HSV color space to pick our random color since
-  // I find it more intuitive than RGB for modiying colors and determining areas of attractive colors
+      util = loadUtilities();
 
+  // First we determine the
+  // hue of our random color
   h = pickHue();
-  sv = pickLuminosity(h); // will return an array with s and v values
-  s = sv[0]; // awkward
-  v = sv[1];
 
-  function pickHue () = {
+  function pickHue () {
     
-    if (argumentsHas('monochrome')) {
-      return 0; // The H value has no bearing on the appearance of a grey
-    }
-
+    // Determine if there's a preference
+    // for the random color's hue
     if (options.hue) {
       
-      // Check if user wants a color with specific h value (e.g. 0 for red)
+      // Determine if the hue preference
+      // is a color name (e.g. 'orange')
+      if (util.colorDictionary.hasOwnProperty(options.hue)) {
+        
+        // Look up the range of hues associated
+        // with the color name (e.g. [18,46] for orange)
+        var hueRange = util.colorDictionary[options.hue].h;
+
+        // Pick a random H value within this range
+        return util.randomBetween(hueRange,'integer')
+
+      } else {
+
+        // The color dictionary doesn't contain the color string
+        console.log('Warning: could not find your hue.')
+      };
+
+      // Determine if the hue preference is a
+      // specific H value (e.g. 0 for red)
       if (typeof options.hue === "number") {
-        if (options.hue <= 360 &&
-            options.hue >= 0) {
+
+        // Ensure H is between 0 and 360
+        if (options.hue <= 360 && options.hue >= 0) {
           return options.hue 
         }
+
+        // Invalid H value
         console.log('Warning: pick H value between 0 and 360')
       }
 
-      // Check if user wants a color with a particular hue (e.g. orange)
-      if (util.colorDictionary[options.hue]) {
-        var hueRange = util.colorDictionary[options.hue].h;
-        return util.randomBetween(hueRange,true)
-      } else {
-        console.log('Warning: could not find your hue.')
+      if (options.hue === 'monochrome') {
+        return 0; // The H value has no effect on the appearance of a grey
       };
       
     };
 
-    // No hue preference detected
-    return util.randomBetween(0,360,true)
+    // No valid hue preference detected, we can pick randomly!
+    return util.randomBetween(0,360,'integer')
   }
 
-  function pickLuminosity (hue) = {
+  l = pickLuminosity(h); // Returns an array with S and V values
+  
+  function pickLuminosity (hue) {
 
     // luminosity is tough to represent as a pair of values but
     // s and v will do for our purposes
     var s,v;
 
-    attractiveTriangle = {
+    attractiveTriangle = [
       [45,100],
       [100,100],
       [100,65]
-    };
+    ];
 
     // this picks a pair of points in rectangle in the upper right of the hsv color space...
-    s = util.randomBetween(45,100,true);
-    v = util.randomBetween(65,100,true);
+    s = util.randomBetween(45,100,'integer');
+    v = util.randomBetween(65,100,'integer');
 
     // but we want points within the attractive triangle...
     // so we'll reflect the points which are generated below the bottom edge of the triangle
 
-
-    if (argumentsHas('monochrome')) {
+    if (options.hue === 'monochrome') {
       
       // s value needs to be zero to produce a grey
       s = 0;
 
       // this should be weighted to higher numbers
-      if (argumentsHas('light')) {
+      if (options.luminosity === 'light') {
         v = util.randomBetween(50,100);
       };
 
       // this should be weighted to lower numbers
-      if (argumentsHas('dark')) {
+      if (options.luminosity === 'dark') {
         v = util.randomBetween(0,50);
       };
-    }
-
-    if (argumentsHas('unweighted')) {
-      s = util.randomBetween(0, 360,true);
-      v = util.randomBetween(0, 360,true);
     }
 
     return [s,v]
 
   }
 
-  function formatColor (h,s,v) = {
+  // Split the luminosity array into S and V values 
+  s = l[0]; 
+  v = l[1];
 
-    // Takes H,S and V values and formats it to the user's preferences
-    // This defaults to a hex string (e.g '#F6F7F8') since it's more widely supported than rgb
+  // Convert the HSV value to an array representing the color in RGB
+  // We do this no matter which format is specified by the user because
+  // Hex is a different way of representing a rgb color
+  
+  console.log('hue preference is: '+ options.hue);
+  console.log('h is: '+ h);
+  console.log('s is: '+ s);
+  console.log('v is: '+ v);
 
-    var rgb = util.hsvRGB(h,s,v),
-        hex = util.rgbHex(rgb); // looks a bit weird how h,s,v are values and rgb is array
 
-    // More broadly I need to work out how to better pass preferences to RandomColor()
-    // Currently you could pass multiple formatting preferences
+  rgb = util.hsvRGB(h,s,v);
 
-    if (argumentsHas('rgb')){
-      return 'rgb(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2]+')'
-    };
+  // Represent the rgb color as a hex string
+  hex = util.rgbHex(rgb); 
 
-    if (argumentsHas('rgbArray')){
-      return rgb // e.g [124,76,200]
-    };
-
-    if (argumentsHas('hsv')){
-      return 'hsv(' + h + ', ' + s + ', ' + v +')'
-    };
-
-    if (argumentsHas('hsvArray')){
-      return [h,s,v] // e.g [124,70,30]
-    };
-
-    return hex
-  }
-
-  var hue = Math.random(),
-      saturation = 0.8,
-      value = 0.9,
-    
-  function argumentsHas  (key) = {
-    for (var i in randomColor.arguments){
-      if (key === randomColor.arguments[i]) {return true}
-    }
-    return false
+  if (options.format === 'rgb'){
+    return 'rgb(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2]+')'
   };
 
+  if (options.format === 'rgbArray'){
+    return rgb // e.g [124,76,200]
+  };
 
-  var util = {
+  if (options.format === 'hsv'){
+    return 'hsv(' + h + ', ' + s + ', ' + v +')'
+  };
+
+  if (options.format === 'hsvArray'){
+    return [h,s,v] // e.g [124,70,30]
+  };
+
+  return hex
+
+};
+
+function loadUtilities () {
+  return {
     goldenRatio: 0.61803398874989,
+    colorDictionary: {
+      red: {
+        h: [0, 18] // oh dear there's also red between 334, 360
+      },
+      orange: {
+        h: [18, 46]
+      },
+      yellow: {
+        h: [46, 66]
+      },
+      green:  {
+        h: [66, 168]
+      },
+      blue: {
+        h: [168, 258]
+        },
+      purple: {
+        h: [258, 282]
+        },
+      pink: {
+        h: [282, 334]
+      }
+    },
     randomBetween: function(min, max, isInteger) {
 
         // this needs to accept multiple arrays too
@@ -149,6 +182,12 @@ var randomColor = function randomColor(options){
          ).slice(-6);
     },
     hsvRGB: function(h,s,v) {
+      
+      // Rebase the h,s,v values
+      var h = h/360,
+          s = s/100,
+          v = v/100;
+
       var h_i = Math.floor(h*6),
         f = h * 6 - h_i,
         p = v * (1 - s),
@@ -168,46 +207,5 @@ var randomColor = function randomColor(options){
       }
       return [Math.floor(r*256), Math.floor(g*256), Math.floor(b*256)];
     }
-  };
-
-  var colorDictionary = {
-    red: {
-      h: [0, 18,
-          334, 360]
-      },
-    orange: {
-      h: [18, 46]
-    },
-    yellow: {
-      h: [46, 66]
-    },
-    green:  {
-      h: [66, 168]
-    },
-    blue: {
-      h: [168, 258]
-      },
-    purple: {
-      h: [258, 282]
-      },
-    pink: {
-      h: [282, 334]
-      },
-  };
-
-  if (argumentsHas('light')){
-    value = 1,
-    saturation = 0.4
-  };
-
-  if (argumentsHas('dark')){
-    value = 0.5,
-    saturation = 1
-  };
-  // if (argumentsHas('dark')){value -= 0.5};
-  // if (argumentsHas('bright')){saturation += 0.5};
-  // if (argumentsHas('dull')){saturation -= 0.5};
-
-  return util.hsvRGB(hue,saturation,value);
-
+  }
 };

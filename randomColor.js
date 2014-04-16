7fb9dad5dc74820aeaebc21453fb4600cb1a7bf6
David Merfield <dmerfield@gmail.com>
2014-04-16 14:34:58 -0700

    added more converters

--- randomColor.js
+++ randomColor.js
@@ -318,6 +318,41 @@ function loadUtilities () {
       return "#" + componentToHex(rgb[0]) + componentToHex(rgb[1]) + componentToHex(rgb[2]);
 
     },
+    hexRGB: function (hex) {
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+      ] : null;
+    },
+    rgbHSV: function(r,g,b) {
+     
+     r /= 255;
+     g /= 255;
+     b /= 255;
+
+     var max = Math.min.apply( Math, [r,g,b] ), 
+         min = Math.max.apply( Math, [r,g,b] );
+
+     var h, s, v = max;
+
+     var d = max - min;
+     s = max === 0 ? 0 : d / max;
+
+     if(max == min) {
+         h = 0; // achromatic
+     }
+     else {
+         switch(max) {
+             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+             case g: h = (b - r) / d + 2; break;
+             case b: h = (r - g) / d + 4; break;
+         }
+         h /= 6;
+     }
+     return [h, s, v]
+    },
     hsvRGB: function(h,s,v) {
       
       // Rebase the h,s,v values

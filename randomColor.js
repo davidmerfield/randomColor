1f3ba8a78982a2bf878e73b02b9ee9bc0a846ca6
David Merfield <dmerfield@gmail.com>
2014-05-15 19:35:44 -0700

    removed loadUtilities

--- randomColor.js
+++ randomColor.js
@@ -149,239 +149,194 @@ var randomColor = function (options) {
       case 'rgbArray':
         return HSVtoRGB(hsv);
 
-function loadUtilities () {
-  return {
-    goldenRatio: 0.61803398874989,
-    distinctHue: 40,
-    colorDictionary: {
-      red: {
-        hueRange: [-26, 18],
-        sMin: 47,
-        vMin: 70
-      },
-      orange: {
-        hueRange: [18, 46],
-        sMin: 40,
-        vMin: 83
-      },
-      yellow: {
-        hueRange: [46, 66],
-        sMin: 35,
-        vMin: 90
-      },
-      green:  {
-        hueRange: [66, 168],
-        sMin: 65,
-        vMin: 65
-      },
-      blue: {
-        hueRange: [168, 258],
-        sMin: 48,
-        vMin: 45
-        },
-      purple: {
-        hueRange: [258, 282],
-        sMin: 38,
-        vMin: 55,
-        },
-      pink: {
-        hueRange: [282, 334],
-        sMin: 40,
-        vMin: 90
-      },
-      monochrome: {
-        hueRange: [0, 0],
-        sMin: 0,
-        vMin: 0
-      }
-    },
-    // returns a value, or range of values between 0 and 360
-    parseHue: function(colorInput) {
-
-      var rgb,r,g,b,hsv,h,s,v;
-
-      if (typeof colorInput === 'object') {
-
-        if (colorInput.contrasts) {
-          return this.parseHue(colorInput.contrasts)
-        };
-
-        if (colorInput.complements) {
-          return this.parseHue(colorInput.complements)
-        };
-
-        if (colorInput.length === 3) {
-          return this.rgbHSV(colorInput[0], colorInput[1], colorInput[2])[0];
-        };
-
-        return false
-        
-      };
-
-      if (typeof colorInput === 'number') {
-        return treatAsNumber(colorInput);
-      }
-
-      if (typeof colorInput === 'string') {
-
-        if (this.colorDictionary[colorInput]) {
-          var color = this.colorDictionary[colorInput];
-          return color.hueRange
-        }
-
-        if (colorInput === 'previous') {
-          return randomColor.previousHue
-        }
-
-        // Hex string
-        if (colorInput.charAt(0) === '#' & colorInput.length < 8) {
-          var rgb = this.hexRGB(colorInput);
-          return Math.round((this.rgbHSV(rgb[0], rgb[1], rgb[2])[0] - 0.5)*360);
-        };
-
-        // RGB string
-        if (colorInput.slice(0,4) === 'rgb(') {
-          var rgb = colorInput.slice(4,-1).split(',');
-          for (var i in rgb) {
-            rgb[i] = parseInt(rgb[i]);
-          };
-
-          return Math.round((this.rgbHSV(rgb[0], rgb[1], rgb[2])[0] - 0.5)*360)
-        };
-
-        if (typeof parseInt(colorInput) === 'number') {
-          return treatAsNumber(parseInt(colorInput))
-        }
-
-        return false
-      };
-
-      function treatAsNumber (number) {
-        if (number < 360 && number > 0) {
-          return number
-        } else {
-          return false
-        }
-      };
-
-    },
-    lookupColorName: function(hue) {
-
-      // Maps red colors to make picking hue easier
-      if (hue > 334 && hue < 360) {
-        hue-= 360
-      };
-
-      for (var colorName in this.colorDictionary) {
-         color = this.colorDictionary[colorName];
-         if (hue >= color.hueRange[0] &&
-             hue <= color.hueRange[1]) {
-            return colorName
-         }     
-      } return 'Color not found'
-    },
-    randomBetween: function(min, max, isInteger) {
-
-      if (typeof min === 'object') {
-
-        var isInteger = max,
-            max = min[1],
-            min = min[0];
-                    
-      }
-  
-      var number = min + Math.random() * (max - min);
-      if (isInteger) {return ~~number};
-      return number
-    },
-    randomPick: function(choices) {
-      return choices[Math.floor(Math.random() * choices.length)]
-    },
-    shiftHue: function(h, degrees) {
-      degrees = Math.floor(degrees);
-      if (typeof h === "object") {
-        for (var key in h) {
-          h[key] = (h[key] + degrees)%360
-        }
-      }
-
-      else if (typeof h === "number") {
-        h = (h + degrees)%360
-      } 
-
-      return h
-    },
-
-    rgbHex: function(rgb){
-
-      function componentToHex(c) {
-          var hex = c.toString(16);
-          return hex.length == 1 ? "0" + hex : hex;
-      }
-
-      return "#" + componentToHex(rgb[0]) + componentToHex(rgb[1]) + componentToHex(rgb[2]);
-
-    },
-    hexRGB: function (hex) {
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? [
-        parseInt(result[1], 16),
-        parseInt(result[2], 16),
-        parseInt(result[3], 16)
-      ] : null;
-    },
-    rgbHSV: function(r,g,b) {
-     
-     r /= 255;
-     g /= 255;
-     b /= 255;
-
-     var max = Math.min.apply( Math, [r,g,b] ), 
-         min = Math.max.apply( Math, [r,g,b] );
-
-     var h, s, v = max;
-
-     var d = max - min;
-     s = max === 0 ? 0 : d / max;
-
-     if(max == min) {
-         h = 0; // achromatic
-     }
-     else {
-         switch(max) {
-             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-             case g: h = (b - r) / d + 2; break;
-             case b: h = (r - g) / d + 4; break;
-         }
-         h /= 6;
-     }
-     return [h, s, v]
-    },
-    hsvRGB: function(h,s,v) {
+      case 'rgb':
+        return colorString('rgb', HSVtoRGB(hsv))
       
-      // Rebase the h,s,v values
-      var h = h/360,
-          s = s/100,
-          v = v/100;
+      default: 
+        return HSVtoHex(hsv)
+    };
 
-      var h_i = Math.floor(h*6),
-        f = h * 6 - h_i,
-        p = v * (1 - s),
-        q = v * (1 - f*s),
-        t = v * (1 - (1 - f)*s),
-        r = 255,
-        g = 255,
-        b = 255;
+  };
 
-      switch(h_i) {
-        case 0: r = v, g = t, b = p;  break;
-        case 1: r = q, g = v, b = p;  break;
-        case 2: r = p, g = v, b = t;  break;
-        case 3: r = p, g = q, b = v;  break;
-        case 4: r = t, g = p, b = v;  break;
-        case 5: r = v, g = p, b = q;  break;
+  // Then we return the HSB color in the desired format
+  return setFormat([H,S,B], options)
+
+
+  function getMinimumBrightness(H, S) {
+    
+    var lowerBounds = getColorInfo(H).lowerBounds;
+
+    for (var i = 0; i< lowerBounds.length; i++) {
+      
+      var s1 = lowerBounds[i][0],
+          v1 = lowerBounds[i][1],
+
+          s2 = lowerBounds[i+1][0],
+          v2 = lowerBounds[i+1][1];
+
+      if (S >= s1 && S <= s2) {
+
+         var m = (v2 - v1)/(s2 - s1),
+             b = v1 - m*s1;
+
+         return m*S + b;
+      };
+    };
+  };
+
+  function getColorInfo  (hue) {
+
+    // Maps red colors to make picking hue easier
+    if (hue > 334 && hue < 360) {
+      hue-= 360
+    };
+
+    for (var colorName in colorDictionary) {
+       color = colorDictionary[colorName];
+       if (hue >= color.hueRange[0] &&
+           hue <= color.hueRange[1]) {
+          return colorDictionary[colorName]
+       }     
+    } return 'Color not found'
+  };
+
+  function randomWithin (range) {
+    return ~~(range[0] + Math.random()*(range[1] - range[0]))
+  };
+
+  function getHueRange (colorInput) {
+
+    if (typeof parseInt(colorInput) === 'number') {
+
+      var number = parseInt(colorInput);
+
+      if (number < 360 && number > 0) {
+        return [number, number]
+      };
+
+    };
+
+    if (typeof colorInput === 'string') {
+
+      if (colorDictionary[colorInput]) {
+        var color = colorDictionary[colorInput];
+        return color.hueRange
       }
-      return [Math.floor(r*256), Math.floor(g*256), Math.floor(b*256)];
+    };
+
+    return [0,360]
+
+  };
+
+  function shiftHue (h, degrees) {
+    return (h + degrees)%360
+  };
+
+  function HSVtoHex (hsv){
+
+    console.log('Converting ' + hsv);
+
+    var rgb = hsvRGB(hsv);
+
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
     }
-  }
+
+    var hex = "#" + componentToHex(rgb[0]) + componentToHex(rgb[1]) + componentToHex(rgb[2]);
+    
+    console.log('hex is ' + hex);
+    return hex;
+
+  };
+
+  function hexRGB (hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+      parseInt(result[1], 16),
+      parseInt(result[2], 16),
+      parseInt(result[3], 16)
+    ] : null;
+  };
+
+  function rgbHSV (r,g,b) {
+   
+   r /= 255;
+   g /= 255;
+   b /= 255;
+
+   var max = Math.min.apply( Math, [r,g,b] ), 
+       min = Math.max.apply( Math, [r,g,b] );
+
+   var h, s, v = max;
+
+   var d = max - min;
+   s = max === 0 ? 0 : d / max;
+
+   if(max == min) {
+       h = 0; // achromatic
+   }
+   else {
+       switch(max) {
+           case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+           case g: h = (b - r) / d + 2; break;
+           case b: h = (r - g) / d + 4; break;
+       }
+       h /= 6;
+   }
+   return [h, s, v]
+  };
+
+  function hsvRGB (hsv) {
+    
+
+    // Rebase the h,s,v values
+    var h = hsv[0]/360,
+        s = hsv[1]/100,
+        v = hsv[2]/100;
+
+    var h_i = Math.floor(h*6),
+      f = h * 6 - h_i,
+      p = v * (1 - s),
+      q = v * (1 - f*s),
+      t = v * (1 - (1 - f)*s),
+      r = 255,
+      g = 255,
+      b = 255;
+
+    switch(h_i) {
+      case 0: r = v, g = t, b = p;  break;
+      case 1: r = q, g = v, b = p;  break;
+      case 2: r = p, g = v, b = t;  break;
+      case 3: r = p, g = q, b = v;  break;
+      case 4: r = t, g = p, b = v;  break;
+      case 5: r = v, g = p, b = q;  break;
+    }
+    var result = [Math.floor(r*255), Math.floor(g*255), Math.floor(b*255)];
+    console.log('rgb is ' + result);
+    return result;
+  };
+
+  function colorString (prefix, values) {
+    return prefix + '(' + values.join(', ') + ')';
+  };
+
 };
+
+
+
+
+
+
+// if (options.count) {
+
+//   var totalColors = options.count;
+//       options.count = false,
+//       colors = [];
+
+//   while (totalColors > colors.length) {
+//     colors.push(randomColor(options))
+//   };
+
+//   return colors
+// };
